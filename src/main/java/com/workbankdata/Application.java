265dc671e2e272b64c363b7367a66847657ac6d1
mark <ulatec@gmail.com>
2017-04-23 14:20:54 -0700

    Country vars, application psvm, comments

--- src/main/java/com/workbankdata/Application.java
+++ src/main/java/com/workbankdata/Application.java
@@ -2,4 +2,7 @@ package com.workbankdata;
 
 
 public class Application {
+    public static void main(String[] args) {
+        //TODO:mbj create main while loop
+    }
 }
--- src/main/java/com/workbankdata/model/Country.java
+++ src/main/java/com/workbankdata/model/Country.java
@@ -2,4 +2,40 @@ package com.workbankdata.model;
 
 
 public class Country {
+    private String code;
+    private String name;
+    private int internetUsers;
+    private int adultLiteracyRate;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getInternetUsers() {
+        return internetUsers;
+    }
+
+    public void setInternetUsers(int internetUsers) {
+        this.internetUsers = internetUsers;
+    }
+
+    public int getAdultLiteracyRate() {
+        return adultLiteracyRate;
+    }
+
+    public void setAdultLiteracyRate(int adultLiteracyRate) {
+        this.adultLiteracyRate = adultLiteracyRate;
+    }
 }
